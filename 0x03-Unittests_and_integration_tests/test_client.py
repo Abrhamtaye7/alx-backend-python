14c7c112c005b5af2d78a00989ded552962af8d1
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2025-11-16 18:54:25 +0300

    fix1

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -31,22 +31,3 @@ def test_org(self, org_name, mock_get_json):
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
-@parameterized.expand([
-    ("google",),
-    ("abc",)
-])
-@patch("client.get_json")  # must patch where it's used
-def test_org(self, org_name, mock_get_json):
-    expected_payload = {"login": org_name}
-    mock_get_json.return_value = expected_payload
-
-    client = GithubOrgClient(org_name)
-    result = client.org  # access property, no parentheses
-
-    self.assertEqual(result, expected_payload)
-    mock_get_json.assert_called_once_with(
-        f"https://api.github.com/orgs/{org_name}"
-    )
