382b471d6dae6180afe1dde5db84015290a6ac67
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2025-11-16 20:54:48 +0300

    fix the test

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,7 +8,7 @@ from unittest.mock import patch, PropertyMock
 from parameterized import parameterized, parameterized_class  # <-- make sure this is here
 from client import GithubOrgClient
 from fixtures import org_payload, repos_payload, expected_repos, apache2_repos
-
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(TestCase):
