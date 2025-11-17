a670ce2991ca97c5ead53d61c2eb6e89ff77aeb7
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2025-11-17 15:04:35 +0300

    django messaging 4

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import User, Conversation, Message
+from .serializers import (
+    UserSerializer,
+    ConversationSerializer,
+    ConversationCreateSerializer,
+    MessageSerializer
+)
 
-# Create your views here.
+# -------------------------
+# Conversation ViewSet
+# -------------------------
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update', 'partial_update']:
+            return ConversationCreateSerializer
+        return ConversationSerializer
+
+    def perform_create(self, serializer):
+        # Optionally attach the creator to participants if needed
+        conversation = serializer.save()
+        if self.request.user not in conversation.participants.all():
+            conversation.participants.add(self.request.user)
+
+    # Optional: List all conversations for the logged-in user
+    def get_queryset(self):
+        user = self.request.user
+        return Conversation.objects.filter(participants=user)
+
+
+# -------------------------
+# Message ViewSet
+# -------------------------
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically set the sender to the logged-in user
+        serializer.save(sender=self.request.user)
+
+    # Optional: Filter messages by conversation
+    def get_queryset(self):
+        conversation_id = self.request.query_params.get('conversation_id')
+        if conversation_id:
+            return Message.objects.filter(conversation_id=conversation_id)
+        return Message.objects.all()
+
