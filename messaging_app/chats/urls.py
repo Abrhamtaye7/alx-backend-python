448fe25de50de4f31823c97746f3fc8df77265b3
Abrhamtaye7 <abrhamtayetessfaye@gmail.com>
2025-11-17 15:23:18 +0300

    urls fix

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include, DefaultRouter
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 from .views import ConversationViewSet, MessageViewSet
